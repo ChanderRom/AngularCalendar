d98d50ddaecbf610baf540fefdd9206a395d6c0c
ChaanChanChan <alexandertakepics@gmail.com>
2023-11-02 12:30:30 +0100

    custom cells started

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,25 +1,8 @@
-import { Component, ChangeDetectionStrategy, ViewChild, TemplateRef } from '@angular/core';
-import { startOfDay, endOfDay, subDays, addDays, endOfMonth, addHours, isSameDay, isSameMonth } from 'date-fns';
+import { Component, ChangeDetectionStrategy, ViewChild, TemplateRef, OnInit, ElementRef, ViewChildren, QueryList } from '@angular/core';
 import { Subject } from 'rxjs';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent, CalendarView } from 'angular-calendar';
-import { EventColor } from 'calendar-utils';
-import flatpickr from "flatpickr";
+import { CalendarEvent, CalendarView } from 'angular-calendar';
 
-const colors: Record<string, EventColor> = {
-  red: {
-    primary: '#ad2121',
-    secondary: '#FAE3E3',
-  },
-  blue: {
-    primary: '#1e90ff',
-    secondary: '#D1E8FF',
-  },
-  yellow: {
-    primary: '#e3bc08',
-    secondary: '#FDF1BA',
-  },
-}
 
 @Component({
   selector: 'app-calendar',
@@ -28,14 +11,94 @@ const colors: Record<string, EventColor> = {
   templateUrl: 'calendar.component.html',
 })
 
-export class CalendarComponent {
 
-  constructor(private modal: NgbModal) {}
+export class CalendarComponent implements OnInit {
+
+  constructor(
+    private modal: NgbModal,
+  ) {}
+
+
+  colors = {
+    red: '#ad2121',
+    yellow: '#FDF1BA',
+    green: '#5fa08a',
+  }
+
+  price!: number
+  
+  
+  @ViewChildren('price') priceElements!: QueryList<ElementRef>
+  
+
+  generateRandomPrice(): number {
+    return this.price = Math.floor(Math.random() * (1200 - 30 + 1)) + 30
+  }
+
+  
+  getPriceColor(price: number) {
+    if (price < 200) {
+      return {
+        'background-color': '#ad2121'
+      }
+    } else if (price >= 200 && price < 500) {
+      return {
+        'background-color': '#FDF1BA'
+      }
+    } else if (price >= 500) {
+      console.log(price)
+      return {
+        'background-color': '#5fa08a'
+      }
+    }
+    return
+  }
+  
+  // getPriceColor(price: number) {
+  //   if (price < 200) {
+  //     return {
+  //       'background-color': this.colors['green']
+  //     }
+  //   } else if (price >= 200 && price < 500) {
+  //     return {
+  //       'background-color': this.colors['yellow']
+  //     }
+  //   } else if (price >= 500) {
+  //     return {
+  //       'background-color': this.colors['red']
+  //     }
+  //   }
+  //   return
+  // }
+  
+  ngAfterViewInit() {
+    this.priceElements.forEach(element => {
+      const value = parseInt(element.nativeElement.textContent)
+
+      this.getPriceColor(value)
+    })
+  }
+  
+  ngOnInit() {
+    this.generateRandomPrice()
+  }
+  
+  
+
+
+
+
+
+
+
+
+
+
+
 
   
   @ViewChild('modalContent', { static: true })
-  modalContent!: TemplateRef<any>;
-  
+  modalContent!: TemplateRef<any>
   
   // PROPERTIES
   modalData!: {
@@ -56,146 +119,146 @@ export class CalendarComponent {
 
 
   // METHODS
-  setView(view: CalendarView) {
-    this.view = view
-  }
-
-
   closeOpenMonthViewDay() {
     this.activeDayIsOpen = false
   }
 
 
-  eventClicked({ event }: { event: CalendarEvent }): void {
-    console.log('Event clicked', event);
-  }
+  // setView(view: CalendarView) {
+  //   this.view = view
+  // }
 
 
-  addEvent(): void {
-    this.events = [
-      ...this.events,
-      {
-        title: 'New event',
-        start: startOfDay(new Date()),
-        end: endOfDay(new Date()),
-        color: colors['red'],
-        draggable: true,
-        resizable: {
-          beforeStart: true,
-          afterEnd: true,
-        },
-      },
-    ]
-  }
 
 
-  deleteEvent(eventToDelete: CalendarEvent) {
-    this.events = this.events.filter((event) => event !== eventToDelete)
-  }
+  // eventClicked({ event }: { event: CalendarEvent }): void {
+  //   console.log('Event clicked', event);
+  // }
 
 
-  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
-    if (isSameMonth(date, this.viewDate)) {
-      if (
-        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
-        events.length === 0
-      ) {
-        this.activeDayIsOpen = false
-      } else {
-        this.activeDayIsOpen = true
-      }
-      this.viewDate = date
-    }
-  }
+  // addEvent(): void {
+  //   this.events = [
+  //     ...this.events,
+  //     {
+  //       title: 'New event',
+  //       start: startOfDay(new Date()),
+  //       end: endOfDay(new Date()),
+  //       color: colors['red'],
+  //       draggable: true,
+  //       resizable: {
+  //         beforeStart: true,
+  //         afterEnd: true,
+  //       },
+  //     },
+  //   ]
+  // }
 
 
-  eventTimesChanged({
-    event,
-    newStart,
-    newEnd,
-  }: CalendarEventTimesChangedEvent): void {
-    this.events = this.events.map((iEvent) => {
-      if (iEvent === event) {
-        return {
-          ...event,
-          start: newStart,
-          end: newEnd,
-        }
-      }
-      return iEvent
-    })
-  }
+  // deleteEvent(eventToDelete: CalendarEvent) {
+  //   this.events = this.events.filter((event) => event !== eventToDelete)
+  // }
 
 
-  handleEvent(action: string, event: CalendarEvent): void {
-    this.modalData = { event, action }
-    this.modal.open(this.modalContent, { size: 'lg' })
-  }
+  // dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+  //   if (isSameMonth(date, this.viewDate)) {
+  //     if (
+  //       (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+  //       events.length === 0
+  //     ) {
+  //       this.activeDayIsOpen = false
+  //     } else {
+  //       this.activeDayIsOpen = true
+  //     }
+  //     this.viewDate = date
+  //   }
+  // }
+
+
+  // eventTimesChanged({
+  //   event,
+  //   newStart,
+  //   newEnd,
+  // }: CalendarEventTimesChangedEvent): void {
+  //   this.events = this.events.map((iEvent) => {
+  //     if (iEvent === event) {
+  //       return {
+  //         ...event,
+  //         start: newStart,
+  //         end: newEnd,
+  //       }
+  //     }
+  //     return iEvent
+  //   })
+  // }
+
+
+  // handleEvent(action: string, event: CalendarEvent): void {
+  //   this.modalData = { event, action }
+  //   this.modal.open(this.modalContent, { size: 'lg' })
+  // }
 
 
   
   // ACTIONS
-  actions: CalendarEventAction[] = [
-    {
-      label: '<i class="bi bi-pencil-fill i-button"></i>',
-      a11yLabel: 'Edit',
-      onClick: ({ event }: { event: CalendarEvent }): void => {
-        this.handleEvent('Edited', event)
-      },
-    },
-    {
-      label: '<i class="bi bi-trash3-fill i-button"></i>',
-      a11yLabel: 'Delete',
-      onClick: ({ event }: { event: CalendarEvent }): void => {
-        this.handleEvent(`Event: ${event.title}, deleted`, event)
-        this.events = this.events.filter((iEvent) => iEvent !== event)
-        this.deleteEvent(event)
-      },
-    },
-  ]
-
+  // actions: CalendarEventAction[] = [
+  //   {
+  //     label: '<i class="bi bi-pencil-fill i-button"></i>',
+  //     a11yLabel: 'Edit',
+  //     onClick: ({ event }: { event: CalendarEvent }): void => {
+  //       this.handleEvent('Edited', event)
+  //     },
+  //   },
+  //   {
+  //     label: '<i class="bi bi-trash3-fill i-button"></i>',
+  //     a11yLabel: 'Delete',
+  //     onClick: ({ event }: { event: CalendarEvent }): void => {
+  //       this.handleEvent(`Event: ${event.title}, deleted`, event)
+  //       this.events = this.events.filter((iEvent) => iEvent !== event)
+  //       this.deleteEvent(event)
+  //     },
+  //   },
+  // ]
 
 
   // MOCK UP EVENTS
-  events: CalendarEvent[] = [
-    {
-      start: subDays(startOfDay(new Date()), 1),
-      end: addDays(new Date(), 1),
-      title: 'A 3 day event',
-      color: { ...colors['red'] },
-      actions: this.actions,
-      allDay: true,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true,
-      },
-      draggable: true,
-    },
-    {
-      start: startOfDay(new Date()),
-      title: 'An event with no end date',
-      color: { ...colors['yellow'] },
-      actions: this.actions,
-    },
-    {
-      start: subDays(endOfMonth(new Date()), 3),
-      end: addDays(endOfMonth(new Date()), 3),
-      title: 'A long event that spans 2 months',
-      color: { ...colors['blue'] },
-      allDay: true,
-    },
-    {
-      start: addHours(startOfDay(new Date()), 2),
-      end: addHours(new Date(), 2),
-      title: 'A draggable and resizable event',
-      color: { ...colors['yellow'] },
-      actions: this.actions,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true,
-      },
-      draggable: true,
-    },
-  ]
-
+//   events: CalendarEvent[] = [
+//     {
+//       start: subDays(startOfDay(new Date()), 1),
+//       end: addDays(new Date(), 1),
+//       title: 'A 3 day event',
+//       color: { ...colors['red'] },
+//       actions: this.actions,
+//       allDay: true,
+//       resizable: {
+//         beforeStart: true,
+//         afterEnd: true,
+//       },
+//       draggable: true,
+//     },
+//     {
+//       start: startOfDay(new Date()),
+//       title: 'An event with no end date',
+//       color: { ...colors['yellow'] },
+//       actions: this.actions,
+//     },
+//     {
+//       start: subDays(endOfMonth(new Date()), 3),
+//       end: addDays(endOfMonth(new Date()), 3),
+//       title: 'A long event that spans 2 months',
+//       color: { ...colors['blue'] },
+//       allDay: true,
+//     },
+//     {
+//       start: addHours(startOfDay(new Date()), 2),
+//       end: addHours(new Date(), 2),
+//       title: 'A draggable and resizable event',
+//       color: { ...colors['yellow'] },
+//       actions: this.actions,
+//       resizable: {
+//         beforeStart: true,
+//         afterEnd: true,
+//       },
+//       draggable: true,
+//     },
+//   ]
 }
